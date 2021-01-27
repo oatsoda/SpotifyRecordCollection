176032cf127981300734937b85516841c05c4be8
oatsoda <andrew+github@milne.tel>
2021-01-27 15:27:46 +0000

    v. rough oauth without refresh

---
+++ spotify-record-collection/src/components/SpotifyAuth.tsx
@@ -0,0 +1,179 @@
+import cryptoRandomString from 'crypto-random-string';
+import sha256 from 'crypto-js/sha256';
+import Base64 from 'crypto-js/enc-base64';
+import React, { useCallback, useEffect, useState } from 'react';
+import Axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+const codeVerifierLength = 128;
+const spotifyClientId = "078e53defda343c19205d805139575e0";
+const callbackRedirect = "http://localhost:3000/";
+const scopes = "user-library-read";
+const exchangeCodeUrl = "https://accounts.spotify.com/api/token";
+
+function SpotifyAuth() {
+
+  const urlParams = new URLSearchParams(window.parent.location.search);
+  const code = urlParams.get('code');
+  const state = urlParams.get('state');
+  const error = urlParams.get('error');
+
+  let [accessDetails, setAccessDetails] = useState<PostExchangeResponse>();
+  let [errorDetails, setErrorDetails] = useState<PostExchangeError>();
+
+  async function postExchangeCode(url: string, code: string, codeVerifier: string) {
+
+    const params = new URLSearchParams();
+    params.append("client_id", spotifyClientId);
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", callbackRedirect);
+    params.append("code_verifier", codeVerifier);
+    
+    const config : AxiosRequestConfig = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      validateStatus: _ => true    
+    }
+  
+    await Axios.post<PostExchangeResponse>(url, params, config)
+      .then(processResponseAxios)
+      .then(result => {
+        console.log(result);
+        setAccessDetails(result.data);
+      })
+      .catch(err => {
+        // Do somthing
+        console.log (err);
+        if (err.status === 400) {
+          setErrorDetails(err.data);
+        }
+      })
+  }
+
+  useEffect(() => {
+
+    if (accessDetails || errorDetails)
+      return;
+
+    if (code != null) {
+
+      const sourceState = localStorage.getItem("authState");
+      if (state !== sourceState) {
+        // TODO: error
+        console.log(`Stored state: ${sourceState}`);
+        console.log(`Returned state: ${state}`);
+        return;
+      }
+
+      // Post exchange of code for Access Token
+      const codeVerifier = localStorage.getItem("authCodeVerifier")!;
+      console.log(`Local Code Verifier: ${codeVerifier}`);
+      postExchangeCode(exchangeCodeUrl, code, codeVerifier);
+    }
+  });
+
+  const onStartAuthenticate = useCallback(async () => {
+
+    const codeVerifier = cryptoRandomString({length: codeVerifierLength});
+    console.log(`New Code Verifier: ${codeVerifier}`);
+    localStorage.setItem("authCodeVerifier", codeVerifier);
+    const codeChallenge = await pkce_challenge_from_verifier(codeVerifier); //encodeURIComponent(sha256(codeVerifier).toString(Base64));
+    console.log(`Code Challenge: ${codeChallenge}`);
+    const sourceState = cryptoRandomString({length: 14, type: 'base64'});
+    localStorage.setItem("authState", sourceState);
+
+    const startAuthUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${spotifyClientId}&redirect_uri=${encodeURIComponent(callbackRedirect)}&state=${sourceState}&code_challenge=${codeChallenge}&code_challenge_method=S256&scope=${scopes}`;
+
+    // Redirect browser to Permissions screen
+    window.location.href = startAuthUrl;
+
+  }, []);
+
+  return (    
+    <div>
+      <p><button onClick={onStartAuthenticate}>Authenticate</button></p>
+
+      <p>Login</p>
+      <p>State: {state}</p>
+      <p>Code: {code}</p>
+      <p>Error: {error}</p>
+      { accessDetails &&
+      <div>
+        <p>{accessDetails.access_token}</p>
+        <p>{accessDetails.expires_in}</p>
+        <p>{accessDetails.refresh_token}</p>
+        <p>{accessDetails.scope}</p>
+        <p>{accessDetails.token_type}</p>
+      </div>
+      }
+      { errorDetails &&
+      <div>
+        <p>{errorDetails.error}</p>
+        <p>{errorDetails.error_description}</p>
+      </div>
+      }
+    </div>
+  )
+}
+
+
+type PostExchangeResponse = {
+  access_token: string,
+  token_type: string,
+  scope: string,
+  expires_in: number,
+  refresh_token: string,
+}
+
+type PostExchangeError = {  
+  error: string,
+  error_description: string,
+}
+
+export function processResponseAxios<T>(response: AxiosResponse<T>): Promise<ProcessedResponseOf<T>> {
+
+  return new Promise((resolve, reject) => {
+    // will resolve or reject depending on status, will pass both "status" and "data" in either case
+    let func: any;
+    console.log(`response status: ${response.status}`);
+    response.status < 400 ? func = resolve : func = reject;
+    func({ ok: response.status >= 200 && response.status < 300, status: response.status, data: response.data });
+  });
+}
+
+export interface ProcessedResponse {
+  ok: boolean, // Whether within 200 range (useful for resolved response which is in 100 or 300)
+  status: number;
+  data: any
+}
+
+export interface ProcessedResponseOf<T> {
+  ok: boolean, // Whether within 200 range (useful for resolved response which is in 100 or 300)
+  status: number;
+  data: T
+}
+
+function sha256_2(plain: string) { 
+  // returns promise ArrayBuffer
+  const encoder = new TextEncoder();
+  const data = encoder.encode(plain);
+  return window.crypto.subtle.digest('SHA-256', data);
+}
+
+function base64urlencode(a: ArrayBuffer) {
+  // Convert the ArrayBuffer to string using Uint8 array.
+  // btoa takes chars from 0-255 and base64 encodes.
+  // Then convert the base64 encoded to base64url encoded.
+  // (replace + with -, replace / with _, trim trailing =)
+  return btoa(String.fromCharCode.apply(null, Array.from(new Uint8Array(a))))
+      .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
+
+async function pkce_challenge_from_verifier(v: string) : Promise<string> {
+  const hashed = await sha256_2(v);
+  const base64encoded = base64urlencode(hashed);
+  return base64encoded;
+}
+
+export { SpotifyAuth };
